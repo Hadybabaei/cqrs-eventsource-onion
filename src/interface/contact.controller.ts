e83319e6df3084c7d11f1e11c19ae134e8cabecf
hadybabaei <mr.hady.ba69@gmail.com>
2024-10-29 16:24:59 +0330

    adding transformer

--- src/interface/contact.controller.ts
+++ src/interface/contact.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { ContactService } from 'src/application/contact.service';
+import { CreateContactDto } from './dto/create-contact.dto';
 
 @Controller('contacts')
 export class ContactController {
   constructor(private readonly contactService: ContactService) {}
 
   @Post()
-  async create(@Body() createContactDto: { name: string; lastname: string }) {
+  @UsePipes(new ValidationPipe({transform:true}))
+  async create(@Body() createContactDto: CreateContactDto) {
     await this.contactService.createContact({
       _id: randomUUID(),
       name: createContactDto.name,
